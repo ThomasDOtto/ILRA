afccbb9048aa31f94b458e7b13c3fd610bf9dcd4
josruirod <joseluis.ruiz@ebd.csic.es>
2021-10-24 14:14:44 +0200

    ABACAS2 and iCORN2 have been updated and adapted

--- external_software/iCORN2/icorn2.serial_bowtie2.sh
+++ external_software/iCORN2/icorn2.serial_bowtie2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-### Modified by José Luis Ruiz to be used as part of the ILRA pipeline in 2022
+### Modified by José Luis Ruiz to improve performarme and to be used as part of the ILRA pipeline in 2022
 
 
 ### Checking and setting arguments by default if not provided
