ff965d61a76821cc07e909b470c46ac51ad034b2
josruirod <joseluis.ruiz@csic.es>
2024-09-19 10:26:32 +0200

    iCORN2 has been updated and adapted to ILRA

--- external_software/iCORN2/icorn2.mapper.sh
+++ external_software/iCORN2/icorn2.mapper.sh
@@ -26,8 +26,14 @@ echo -e "Current cores: $cores\n"
 
 if [ $low_spa_mode == "yes" ]; then
 	output_folder=/dev/shm
+	echo -e "\n You have selected low_spa mode for iCORN2. Accordingly, some steps are going to be performed in /dev/shm, which will increase memory usage or maybe not work in some systems..."
 else
 	output_folder=$PWD
+fi
+
+if [ $low_mem_mode == "yes" ]; then
+	cores=$((cores / 2))
+	echo -e "\n You have selected low_memory mode for iCORN2. Accordingly, Bowtie2 and MarkDuplicatesSpark will be executed using half the cores to try and limit memory usage. If still running into memory issues, try manually decreasing even more..."
 fi
 
 samtools faidx $genome 
@@ -35,13 +41,8 @@ mkdir -p $output_folder/$resultname; mkdir -p $resultname
 
 ### Executing bowtie2...
 echo -e "\nCalling Bowtie2..."
-if [ $low_mem_mode == "yes" ]; then
-	bowtie2-build --threads $((cores / 2)) $genome $output_folder/$genome &> $resultname/bowtiebuild.log_out.txt
-	bowtie2 -t -x $output_folder/$genome -p $((cores / 2)) -X $insertSize --very-sensitive -N 1 -L 31 --rdg 5,2 -1 "$readRoot"_1.fastq -2 "$readRoot"_2.fastq | awk -va=$readRoot '{if ($1~/^@/) {print} else {print $0"\tRG:Z:"a}}' | samtools view -@ $((cores / 2)) -f 0x2 -u -t $genome.fai - | samtools reheader -c 'sed "$ a\@RG\tID:$readRoot\tSM:1"' - | samtools sort -@ $((cores / 2)) -n -l 9 -m 2G -o $output_folder/$resultname/out.bam -
-elif [ $low_mem_mode == "no" ]; then
-	bowtie2-build --threads $cores $genome $output_folder/$genome &> $resultname/bowtiebuild.log_out.txt
-	bowtie2 -t -x $output_folder/$genome -p $cores -X $insertSize --very-sensitive -N 1 -L 31 --rdg 5,2 -1 "$readRoot"_1.fastq -2 "$readRoot"_2.fastq | awk -va=$readRoot '{if ($1~/^@/) {print} else {print $0"\tRG:Z:"a}}' | samtools view -@ $cores -f 0x2 -u -t $genome.fai - | samtools reheader -c 'sed "$ a\@RG\tID:$readRoot\tSM:1"' - | samtools sort -@ $cores -n -l 9 -m 2G -o $output_folder/$resultname/out.bam -
-fi
+bowtie2-build --threads $cores $genome $output_folder/$genome &> $resultname/bowtiebuild.log_out.txt
+bowtie2 -t -x $output_folder/$genome -p $cores -X $insertSize --very-sensitive -N 1 -L 31 --rdg 5,2 -1 "$readRoot"_1.fastq.gz -2 "$readRoot"_2.fastq.gz | awk -va=$readRoot '{if ($1~/^@/) {print} else {print $0"\tRG:Z:"a}}' | samtools view -@ $cores -f 0x2 -u -t $genome.fai - | samtools reheader -c 'sed "$ a\@RG\tID:$readRoot\tSM:1"' - | samtools sort -@ $((cores / 2)) -n -l 9 -m 10G -o $output_folder/$resultname/out.bam -
 rm $output_folder/$genome.* # Remove the bowtie2 index
 echo -e "\nBowtie2 DONE"
 
@@ -50,17 +51,16 @@ echo -e "\nCalling MarkDuplicatesSpark (GATK)..."
 # Add the argument -M marked_dup_metrics.txt if you want to gather the stats, but by default omitted as it slows down
 echo -e "\nThe use of OpenJDK v8 is going to try and be forced, by adding to the PATH once... but custom paths may be required here...\n"
 PATH=$ICORN2_HOME/jdk8u302-b08/bin:$PATH
-if [ $low_mem_mode == "yes" ]; then
-	$ICORN2_HOME/gatk MarkDuplicatesSpark -I $output_folder/$resultname/out.bam -O $output_folder/$resultname/out.sorted.markdup.bam -VS SILENT -OBI true --create-output-bam-splitting-index false --tmp-dir $TMPDIR --spark-master local[$((cores / 2))] &> $resultname/MarkDuplicatesSpark_log.out.txt
-	echo -e "\n You have selected low_memory mode for iCORN2. Accordingly, Bowtie2 and MarkDuplicatesSpark have been executed using half the cores to try and limit memory usage. If still running into memory issues, try manually decreasing even more..."
-elif [ $low_mem_mode == "no" ]; then
-	$ICORN2_HOME/gatk MarkDuplicatesSpark -I $output_folder/$resultname/out.bam -O $output_folder/$resultname/out.sorted.markdup.bam -VS SILENT -OBI true --create-output-bam-splitting-index false --tmp-dir $TMPDIR --spark-master local[$cores] &> $resultname/MarkDuplicatesSpark_log.out.txt
-fi
+$ICORN2_HOME/gatk MarkDuplicatesSpark -I $output_folder/$resultname/out.bam -O $output_folder/$resultname/out.sorted.markdup.bam -VS SILENT -OBI true --create-output-bam-splitting-index false --tmp-dir $TMPDIR --spark-master local[$((cores / 2))] &> $resultname/MarkDuplicatesSpark_log.out.txt
+
+### Ending:
 return_markdup=$?
 export return_markdup
 if [ "$return_markdup" != "0" ] ; then
 	echo -e "\nSorry, MarkDuplicatesSpark failed... Maybe due to excessive memory required? (consider using less cores, setting up Xmx in _JAVA_OPTIONS variable?). Maybe due to excessive open files (see ulimit -a)?. Maybe due to incorrect JAVA version (OpenJDK v8 is strictly required by GATK, and has to be in the PATH, double check the PATH)? Please check the log MarkDuplicatesSpark_log.out.txt to find more information...\n"
 	exit 1;
 else
-	rm $output_folder/$resultname/out.bam; rm -rf $TMPDIR/*; echo -e "\nMarkDuplicates DONE"
+	rm $output_folder/$resultname/out.bam
+	rm -rf $TMPDIR/*
 fi
+echo -e "\nMarkDuplicates DONE"
