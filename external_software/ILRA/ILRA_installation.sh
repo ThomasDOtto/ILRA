b100c1b4a5ae21d3921501278ccd221663d44688
josruirod <joseluis.ruiz@ebd.csic.es>
2026-04-20 07:59:13 +0200

    updating installation, outdated links, defaults conda channel, etc

--- external_software/ILRA/ILRA_installation.sh
+++ external_software/ILRA/ILRA_installation.sh
@@ -1,13 +1,81 @@
 #!/bin/bash
 
-echo -e "\nThis is a wrapper script to install and link all ILRA dependencies available through conda (multiple environments due to dependency conflicts) and others external, such as iCORN2, jvarkit, outdated java...\n"
+echo -e "\nThis is a wrapper script to install and link all ILRA dependencies available through conda (multiple environments due to dependency conflicts) and others external, such as iCORN2, jvarkit, outdated java...\n" && date
 
-#### Get the directory of the script:
+
+#### 0. Get the directory of the script:
 EXTERNAL_SOFTWARE_DIR=$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))
-echo "The folder with external software is $EXTERNAL_SOFTWARE_DIR"
+echo -e "\nThe folder with external software is $EXTERNAL_SOFTWARE_DIR\n"
 
-#### Setting permissions of executables:
-echo -e "\n\nSetting permissions of executables in $EXTERNAL_SOFTWARE_DIR...\n\n"
+#### 1. Download and install Miniconda if required:
+if type mamba >/dev/null 2>&1; then
+    echo "Mamba found. Using mamba for installation..."
+    CONDA_INSTALL="mamba"
+elif type conda >/dev/null 2>&1; then
+    echo "Mamba not found, but conda is available. Using conda for installation..."
+    CONDA_INSTALL="conda"
+else
+    echo >&2 "Conda/Mamba is required... and neither executable was found in the PATH."
+    echo >&2 "Conda installation within ILRA folder ($EXTERNAL_SOFTWARE_DIR/miniconda3) is going to happen in 30 seconds."
+    echo >&2 "Please kill the process (Ctrl+C) and correct the PATH if not necessary to install miniconda, otherwise the installation will continue in the folder $EXTERNAL_SOFTWARE_DIR/miniconda3 and create there ILRA environments\n\n\n"
+    echo -e "\nIf for some reason an outdated python or conda is required in your system, please kill this process and go to https://repo.anaconda.com/miniconda/ to download and install manually the corresponding Linux installer\n"
+
+    # 30 second countdown timer
+    secs=30
+    while [ $secs -gt 0 ]; do
+       echo -ne "$secs\033[0K\r"
+       sleep 1
+       : $((secs--))
+    done
+
+    echo -e "\nProceeding with conda installation...\n"
+
+    # Create directory and download installer without changing working directories
+    mkdir -p "$EXTERNAL_SOFTWARE_DIR"
+    wget -qO Miniconda3-latest-Linux-x86_64.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+    bash Miniconda3-latest-Linux-x86_64.sh -b -f -s -p "$EXTERNAL_SOFTWARE_DIR/miniconda3"
+    rm Miniconda3-latest-Linux-x86_64.sh
+
+    # Check if installation was successful
+    if [[ ! -f "$EXTERNAL_SOFTWARE_DIR/miniconda3/bin/conda" ]]; then
+        echo "Installation of Miniconda3 failed, please check manually... Exiting..."
+        exit 1
+    else
+        # Prepend the newly installed conda to the PATH
+        export PATH="$PWD/$EXTERNAL_SOFTWARE_DIR/miniconda3/bin:$PATH"
+    fi
+
+    # Install mamba into the base environment to speed up future installations
+    conda install -y -q -c conda-forge mamba
+
+    # Set the variable to use mamba for the final step
+    CONDA_INSTALL="mamba"
+fi
+
+#### 2. Install packages via conda/mamba:
+echo -e "\n\nInstalling dependencies through conda/mamba...\n\n"
+echo -e "Populating the environments based on the .yml files (keep in mind, this is frozen versions of the software)..."
+echo -e "The pathway to conda environments is $EXTERNAL_SOFTWARE_DIR/ILRA/...\n"
+$CONDA_INSTALL clean -afyq
+echo -e "\n\nInstalling ILRA_env...\n\n" && date
+$CONDA_INSTALL env create -c conda-forge -c bioconda -yq --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_1.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env && echo -e "\n\nDONE... ILRA_env\n\n" && date || { echo >&2 "FATAL ERROR: Failed to install ILRA_env. Exiting script."; exit 1; }
+echo -e "\n\nInstalling ILRA_env_busco...\n\n" && date
+$CONDA_INSTALL env create -c conda-forge -c bioconda -yq --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_2.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_busco && echo -e "\n\nDONE... ILRA_env_busco\n\n" && date || { echo >&2 "FATAL ERROR: Failed to install ILRA_env. Exiting script."; exit 1; }
+echo -e "\n\nInstalling ILRA_env_quast...\n\n" && date
+$CONDA_INSTALL env create -c conda-forge -c bioconda -yq --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_3.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_quast && echo -e "\n\nDONE... ILRA_env_quast\n\n" && date || { echo >&2 "FATAL ERROR: Failed to install ILRA_env. Exiting script."; exit 1; }
+echo -e "\n\nInstalling ILRA_env_syri...\n\n" && date
+$CONDA_INSTALL env create -c conda-forge -c bioconda -yq --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_4.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_syri && echo -e "\n\nDONE... ILRA_env_syri\n\n" && date || { echo >&2 "FATAL ERROR: Failed to install ILRA_env. Exiting script."; exit 1; }
+$CONDA_INSTALL clean -afyq
+echo -e "\n\nInstalling bioconductor cogeqc and blobtoolkit...\n\n" && date
+$EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_busco/bin/Rscript -e 'BiocManager::install("cogeqc")' # A package not in conda as of yet
+
+export PATH=$EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env/bin/:$PATH
+pip -q install "blobtoolkit[full]"
+echo -e "\n\nDONE... cogeqc/blobtoolkit\n\n" && date
+
+
+#### 3. Setting permissions of software included in the external_software_dir:
+echo -e "\n\nSetting permissions in $EXTERNAL_SOFTWARE_DIR...\n\n"
 # (https://github.com/oXis/gtool)
 chmod 775 $EXTERNAL_SOFTWARE_DIR/gtool.py
 # ftp://ftp.ncbi.nlm.nih.gov/blast/demo/vecscreen
@@ -22,53 +90,8 @@ cd $EXTERNAL_SOFTWARE_DIR/ABACAS2 && chmod 775 *
 # (https://sourceforge.net/projects/icorn/files/, but the version here is updated and better optimized)
 cd $EXTERNAL_SOFTWARE_DIR/iCORN2 && chmod 775 *
 
-#### Download and install Miniconda if required:
-export conda_install=""
-type conda >/dev/null 2>&1 || { echo >&2 "Conda is required to be installed... and the 'conda' executable is not being found in the PATH. Conda installation within ILRA folder (external_software/miniconda3) is going to happen in 30 seconds. Please kill the process and correct the PATH if not necessary, otherwise let the installation continue..."; export conda_install="yes"; }
 
-if [ "$conda_install" == "yes" ]; then
-	echo -e "\n\n\nI'm downloading and installing an updated copy of Miniconda3 in the folder external_software/miniconda3 to create ILRA environments\n\n\n"
-	echo -e "\nIf for some reason an outdated python or conda is required in your system, please kill this process and and go to https://repo.anaconda.com/miniconda/ to download and install manually the corresponding Linux installer\n"
-	secs=$((1 * 30))
-	while [ $secs -gt 0 ]; do
-	   echo -ne "$secs\033[0K\r"
-	   sleep 1
-	   : $((secs--))
-	done
-	echo -e "\nProceeding with conda installation\n"
-	cd $EXTERNAL_SOFTWARE_DIR && wget -q "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-	mkdir -p miniconda3 && bash Miniconda3-latest-Linux-x86_64.sh -b -f -s -p $EXTERNAL_SOFTWARE_DIR/miniconda3 && rm Miniconda3-latest-Linux-x86_64.sh
-	if [[ ! -f $EXTERNAL_SOFTWARE_DIR/miniconda3/bin/conda ]]; then
-		echo "Installation of Miniconda3 failed, please check manually... Exiting..."
-   		exit 1
-	else
-		export PATH=$EXTERNAL_SOFTWARE_DIR/miniconda3/bin:$PATH
-	fi
-fi
-
-#### Install packages via conda:
-echo -e "\n\nInstalling dependencies through conda...\n\n"
-echo -e "First mamba replacing conda to reduce time, and then populating the environments based on the .yml files (keep in mind, this is frozen versions of the software)..."
-echo -e "The pathway to conda environments is $EXTERNAL_SOFTWARE_DIR/ILRA/...\n"
-export PATH=$(conda env list | grep -v "#" | head -1 | sed 's,.* ,,g')/bin:$PATH
-conda install -y -q -c conda-forge mamba
-echo -e "\n\nInstalling ILRA_env...\n\n"
-mamba clean -afyq && mamba env create -q --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_1.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env && echo -e "\n\nDONE... ILRA_env\n\n"
-echo -e "\n\nInstalling ILRA_env_busco...\n\n"
-mamba clean -afyq && mamba env create -q --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_2.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_busco && echo -e "\n\nDONE... ILRA_env_busco\n\n"
-echo -e "\n\nInstalling ILRA_env_quast...\n\n"
-mamba clean -afyq && mamba env create -q --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_3.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_quast && echo -e "\n\nDONE... ILRA_env_quast\n\n"
-echo -e "\n\nInstalling ILRA_env_syri...\n\n"
-mamba clean -afyq && mamba env create -q --file $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_4.yml --prefix $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_syri && echo -e "\n\nDONE... ILRA_env_syri\n\n" && mamba clean -afyq
-echo -e "\n\nInstalling bioconductor cogeqc and blobtoolkit...\n\n"
-$EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_busco/bin/Rscript -e 'BiocManager::install("cogeqc")' # A package not in conda as of yet
-# rm -rf $(find $(which conda | sed 's,/bin/conda,,g') -type d -name pkgs) # Remove temp files
-# Not required anymore, I'm using mamba clean above
-export PATH=$EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env/bin/:$PATH
-pip -q install "blobtoolkit[full]"
-echo -e "\n\nDONE...\n\n"
-
-#### Manual fixes
+#### 4. Manual fixes
 echo -e "\n\n\nManually fixing some dependencies and soft linking so only one conda environment has to be activated / only one directory has to be included in the PATH...\n\n\n"
 ## Fix canu and nucmer detection by circlator and spades 3.15.3 not working with modern version of python3
 sed -i 's,Canu,canu,g' $(find $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env -name external_progs.py)
@@ -95,7 +118,8 @@ sed -i "s,x/g' },x/g'\n},g" temp
 sed -i 's,; export,\nexport,g' temp
 rm mask_low_complexity.sh; mv temp mask_low_complexity.sh; chmod 775 mask_low_complexity.sh
 
-#### Soft linking to the main directory:
+
+#### 5. Soft linking to the main directory:
 cd $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env/bin/
 ln -sf $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_busco/bin/busco busco
 ln -sf $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_busco/bin/metaeuk metaeuk
@@ -122,25 +146,27 @@ ln -sf $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env_syri/bin/time time
 # cd $(dirname $(find $EXTERNAL_SOFTWARE_DIR -name misc.py | grep ra_utils | grep /envs/quast/)) && rm misc.py && wget "https://raw.githubusercontent.com/ablab/quast/master/quast_libs/ra_utils/misc.py"
 # cd $(dirname $(find $EXTERNAL_SOFTWARE_DIR -name reads_analyzer.py | grep /envs/quast/)) && rm reads_analyzer.py && wget "https://raw.githubusercontent.com/ablab/quast/master/quast_libs/reads_analyzer.py"
 
-#### Install jvarkit:
+
+#### 6. Install more software:
+## Install jvarkit:
 echo -e "\n\n\nI'm installing jvarkit v2021.10.13 to match java in ILRA_env...\n\n\n"
 git clone https://github.com/lindenb/jvarkit.git && cd jvarkit && git checkout 2021.10.13
 JAVA_HOME=$EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env
 ./gradlew -q samfixcigar && mv dist/samfixcigar.jar ../samfixcigar.jar && cd $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env/bin/ && rm -rf jvarkit
 
-#### Install bbtools:
+## Install bbtools:
 echo -e "\n\n\nI'm installing bbtools...\n\n\n"
 wget -q https://downloads.sourceforge.net/project/bbmap/BBMap_39.01.tar.gz; tar -xzf BBMap_39.01.tar.gz && rm BBMap_39.01.tar.gz
 ln -sf $(find bbmap/ -name "*.sh") .
 
-#### Install assembly-stats graphical:
+## Install assembly-stats graphical:
 echo -e "\n\n\nI'm installing assembly-stats graphical...\n\n\n"
 wget -q https://github.com/rjchallis/assembly-stats/archive/refs/tags/17.02.tar.gz && tar -xzf 17.02.tar.gz && rm 17.02.tar.gz
 ln -sf assembly-stats-17.02/pl/asm2stats.* .
 chmod 775 asm2stats.*
 sed -i 's,/usr/bin/perl -w,/usr/bin/env perl,g' asm2stats.*
 
-#### Install NGenomeSyn:
+## Install NGenomeSyn:
 echo -e "\n\n\nI'm installing NGenomeSyn...\n\n\n"
 wget -q https://github.com/hewm2008/NGenomeSyn/archive/v1.41.tar.gz && tar -xzf v1.41.tar.gz && rm v1.41.tar.gz
 chmod -R 775 NGenomeSyn-1.41/bin/*
@@ -148,28 +174,25 @@ ln -sf NGenomeSyn-1.41/bin/GetTwoGenomeSyn.pl .; ln -sf NGenomeSyn-1.41/bin/NGen
 sed -i 's,/usr/bin/perl -w,/usr/bin/env perl,g' GetTwoGenomeSyn.pl
 sed -i 's,/usr/bin/perl -w,/usr/bin/env perl,g' NGenomeSyn
 
-#### Install iCORN2:
+## Install iCORN2:
 echo -e "\n\n\nI'm installing iCORN2...\n\n\n"
 cd $EXTERNAL_SOFTWARE_DIR/iCORN2
-## Getting SNP-o-matic: 
+## Getting SNP-o-matic:
 echo -e "I'm installing iCORN2...Getting SNP-o-matic"
 ln -sf $(which findknownsnps) findknownsnps
-## Getting FASTA Splitter: 
+## Getting FASTA Splitter:
 echo -e "I'm installing iCORN2...Getting FASTA Splitter"
 ln -sf $(which fasta-splitter) fasta-splitter
 ## Getting GATK:
 echo -e "I'm installing iCORN2...Getting GATK"
 ln -sf $(which gatk) gatk
-# $EXTERNAL_SOFTWARE_DIR/iCORN2/gatk --version
 ## Getting picard:
 echo -e "I'm installing iCORN2...Getting Picard"
 ln -sf $(find $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env -name picard.jar | head -1) picard.jar
 ln -sf $(which picard) picard
-# picard MarkDuplicates --version
 ## Getting the outdated java version required for iCORN2:
 echo -e "I'm installing iCORN2...Getting JAVA v1.7 required by iCORN2's SNP caller"
-wget -q http://mirrors.eflycloud.com/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz && tar -xzf jdk-7u80-linux-x64.tar.gz && rm jdk-7u80-linux-x64.tar.gz
-#$EXTERNAL_SOFTWARE_DIR/iCORN2/jdk1.7.0_80/bin/java -version
+wget -q https://repo.huaweicloud.com/java/jdk/7u80-b15/jdk-7u80-linux-x64.tar.gz && tar -xzf jdk-7u80-linux-x64.tar.gz && rm jdk-7u80-linux-x64.tar.gz
 if [[ ! -f $EXTERNAL_SOFTWARE_DIR/iCORN2/jdk1.7.0_80/bin/java ]]; then
   echo "Download/Installation of java failed, please check manually the script and logs..."
   exit 1
@@ -179,13 +202,12 @@ cd $EXTERNAL_SOFTWARE_DIR/iCORN2/jdk1.7.0_80/bin && chmod 775 *
 echo -e "I'm installing iCORN2...Getting JAVA v1.8 required by GATK v4. This one must be in the PATH when executing iCORN2"
 cd $EXTERNAL_SOFTWARE_DIR/iCORN2
 wget -q https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz && tar -xzf OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz && rm OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz
-#$EXTERNAL_SOFTWARE_DIR/iCORN2/jdk8u302-b08/bin/java -version
 if [[ ! -f $EXTERNAL_SOFTWARE_DIR/iCORN2/jdk8u302-b08/bin/java ]]; then
   echo "Download/Installation of java failed, please check manually the script and logs..."
   exit 1
 fi
 cd $EXTERNAL_SOFTWARE_DIR/iCORN2/jdk8u302-b08/bin && chmod 775 *
 
-echo -e "\n\n\nALL DONE. You must make sure that the directory $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env/bin/ is in the PATH before ILRA execution\n\n\n"
+echo -e "\n\n\nALL DONE. You must make sure that the directory $EXTERNAL_SOFTWARE_DIR/ILRA/ILRA_env/bin/ is in the PATH before ILRA execution\n\n\n" && date
 
 
