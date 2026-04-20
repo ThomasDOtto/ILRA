b117521aa49732279e128bb66eda9304103105f5
josruirod <joseluis.ruiz@csic.es>
2026-04-20 12:51:10 +0200

    Update ILRA.removesmalls.R

--- bin/ILRA.removesmalls.R
+++ bin/ILRA.removesmalls.R
@@ -17,11 +17,11 @@ Q1 <- quantile(data$Value, 0.25); Q3 <- quantile(data$Value, 0.75); data$Value_K
 p <- ggplot(data, aes(x = "", y = Value_Kbp)) + geom_violin(fill = "lightblue") + geom_boxplot(width = 0.1, fill = "white", color = "black") +
 annotate("text", x = 0.2, y = Q1/1000, label = paste("Q1: ", round(Q1, 2), " bp"), hjust=0) + annotate("text", x = 0.2, y = Q3/1000, label = paste("Q3: ", round(Q3, 2), " bp"), hjust=0) +
 theme_classic() + theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) + ggtitle(paste0(dim(data)[1]," contigs length distribution"))
-pdf("Contig_length_distribution.pdf");print(p);dev.off()
+pdf("Contig_length_distribution.pdf");print(p);invisible(dev.off())
 p2 <- ggplot(data, aes(x = "", y = log2(Value_Kbp))) + geom_violin(fill = "lightblue") + geom_boxplot(width = 0.1, fill = "white", color = "black") +
 annotate("text", x = 0.2, y = log2(Q1/1000), label = paste("Q1: ", round(Q1, 2), " bp"), hjust=0) + annotate("text", x = 0.2, y = log2(Q3/1000), label = paste("Q3: ", round(Q3, 2), " bp"), hjust=0) +
 theme_classic() + theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) + ggtitle(paste0(dim(data)[1]," contigs length distribution"))
-pdf("Contig_length_distribution_log2.pdf");print(p2);dev.off()
+pdf("Contig_length_distribution_log2.pdf");print(p2);invisible(dev.off())
 
 
 print("Plotting contigs length distribution after filtering")
@@ -30,16 +30,16 @@ Q1 <- quantile(data$Value, 0.25); Q3 <- quantile(data$Value, 0.75); data$Value_K
 p <- ggplot(data, aes(x = "", y = Value_Kbp)) + geom_violin(fill = "lightblue") + geom_boxplot(width = 0.1, fill = "white", color = "black") +
 annotate("text", x = 0.2, y = Q1/1000, label = paste("Q1: ", round(Q1, 2), " bp"), hjust=0) + annotate("text", x = 0.2, y = Q3/1000, label = paste("Q3: ", round(Q3, 2), " bp"), hjust=0) +
 theme_classic() + theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) + ggtitle(paste0(dim(data)[1]," contigs length distribution"))
-pdf("Contig_length_distribution_after_filtering.pdf");print(p);dev.off()
+pdf("Contig_length_distribution_after_filtering.pdf");print(p);invisible(dev.off())
 p2 <- ggplot(data, aes(x = "", y = log2(Value_Kbp))) + geom_violin(fill = "lightblue") + geom_boxplot(width = 0.1, fill = "white", color = "black") +
 annotate("text", x = 0.2, y = log2(Q1/1000), label = paste("Q1: ", round(Q1, 2), " bp"), hjust=0) + annotate("text", x = 0.2, y = log2(Q3/1000), label = paste("Q3: ", round(Q3, 2), " bp"), hjust=0) +
 theme_classic() + theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) + ggtitle(paste0(dim(data)[1]," contigs length distribution"))
-pdf("Contig_length_distribution_log2_after_filtering.pdf");print(p2);dev.off()
+pdf("Contig_length_distribution_log2_after_filtering.pdf");print(p2);invisible(dev.off())
 
 
 print("Aligning the discarded sequences to the filtered assembly to assess redundancy...")
 # Index the filtered assembly:
-system(paste0('cd ',path,' && bowtie2-build --threads ',cores,' ',new_assembly,' tmp &> tmp.log && echo -e "Bowtie2 alignment of the discarded contigs back to the filtered assembly:" &>> Contigs_length_stats.txt'))
+system(paste0('cd ',path,' && bowtie2-build --threads ',cores,' ',new_assembly,' tmp &> tmp.log && echo "Bowtie2 alignment of the discarded contigs back to the filtered assembly:" &>> Contigs_length_stats.txt'))
 # Make the previous assembly single line, extract the discarded contigs, concatenate in a single line, create a multifasta with 75 bp chunks
 system(paste0('cd ',path,' && awk \'/^>/{if(NR>1) printf("\\n"); printf("%s\\t",$0);next;} {printf("%s",$0);} END {printf("\\n");}\' ',prev_assembly,' | tr "\\t" "\\n" | egrep -A1 $(echo ',paste(contigs_short,collapse=" "),' | tr " " "|") | egrep -v "^-" | grep -v ">" | tr -d "\\n" | fold -w 75 | awk \'{print ">chunk_" NR "\\n" $0}\' > tmp.fasta'))
 # Align the discarded sequences (75 bp chunks) to the assembly, remove everything afterwards but the file Contigs_length_stats.txt will contain the alignment ratio:
